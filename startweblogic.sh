4d760050578c06703bdb666a99554e25f3774aa1
marconi86 <39100934+marconi86@users.noreply.github.com>
2018-05-19 17:14:43 -0300

    Update startweblogic.sh

--- startweblogic.sh
+++ startweblogic.sh
@@ -1,4 +1,7 @@
+#!/bin/bash
+#para iniciar corretamente digite o dominio a qual você quer inicializar
+
 echo "weblogic on-line"
 
 cd /home/oracle/Oracle/Middleware/Oracle_Home/user_projects/domains/$1
-./startWebLogic.sh 
+./startWebLogic.sh
